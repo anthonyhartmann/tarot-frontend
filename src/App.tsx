687005646f7e356b13a14be969f60a1dc469315d
anthonyhartmann <anthonyhartmann395@gmail.com>
2023-12-01 10:31:49 -0800

    Taking this one on faith

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,17 @@ import logo from './logo.svg';
 import './App.css';
 import { Card } from './types';
 import TarotCard from './components/TarotCard/TarotCard';
+import InfoDump from './components/InfoDump/InfoDump';
+import { act } from 'react-dom/test-utils';
 
-const STEP_NUMBER_MAP = ['the querent', 'the block', 'the root', 'the recent past', 'possibilities', 'where you\'re headed', 'how you view yourself', 'your environment', 'hopes and fears', 'the outcome']
+const STEP_NUMBER_MAP = ['the querent', 'the block', 'the root', 'the recent past', 'possibilities', 'where you\'re headed', 'how you view yourself', 'your environment', 'hopes and fears', 'the outcome', 'the end']
 
 function App() {
   const [activeCardModal, setActiveCardModal] = React.useState(-1)
+  const [step, setStep] = React.useState(0)
   const [data, setData] = React.useState([])
+  const [layout, setLayout] = React.useState("celtic-cross")
+  const textMap = new Map<Number, Card>()
   useEffect(() => {
     fetch("/data").then((res) =>
       res.json().then((data) => {
@@ -18,18 +23,24 @@ function App() {
   }, []);
 
   const cardData = data.map((card: Card, index: number) => {
+    textMap.set(index, card)
     return (
       <div>
-        <h2 className='stepTitle'>{STEP_NUMBER_MAP[index]}</h2>
-        <TarotCard index={index} card={card} activeCardModal={activeCardModal} setActiveCardModal={setActiveCardModal}/>
+        <TarotCard index={index} step={step} setStep={setStep} layout={layout} card={card} activeCardModal={activeCardModal} setActiveCardModal={setActiveCardModal}/>
       </div>
     )
   }
   )
-
+  
   return (
     <div className="App">
-      {cardData}
+        <header className="header">
+          {"Step " + step + ":\n" +  STEP_NUMBER_MAP[step]}
+        </header>
+        {cardData}
+        <div className="infoBox">
+          {activeCardModal != -1 && <InfoDump card={textMap.get(activeCardModal)!!}/>}
+        </div>
     </div>
   );
 }
